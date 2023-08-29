b8a7c936913baaaa6ce51f448e9435d46622b25e
georgeuyi <omoragbongeorge@gmaile.com>
2023-08-29 08:36:16 +0100

    All files.

---
+++ src/main/java/com/george/springboot/todoapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,57 @@
+package com.george.springboot.todoapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        UserDetails firstUserDetails = createNewUser("uyi-project", "webmail");
+        UserDetails secondUserDetails = createNewUser("georgeman", "mangeorge");
+
+
+        return new InMemoryUserDetailsManager(firstUserDetails,secondUserDetails);
+    }
+
+    private UserDetails createNewUser(String username, String webmail) {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(webmail)
+                .roles("USER", "ADMIN")
+                .build();
+        return userDetails;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
+    }
+
+}
